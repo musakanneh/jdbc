04c29e5284f3a2232e3c19e192cafa45c7c6607d
Musa Selekie Kanneh <musas.kanneh@gmail.com>
2024-01-14 10:46:17 +0000

    connect to the database

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# jdbc
+# Java Database Connectivity
+## Steps for connecting to postgres database
+1. Import the package
+2. Load and register the package
+3. Create the connection
+4. Create the statement
+5. Execute the statement
+6. Process the results
+7. Close the connection
--- src/JDBCDemo.java
+++ src/JDBCDemo.java
@@ -4,11 +4,18 @@ public class JDBCDemo {
     public static void main(String[] args) throws Exception {
         String url = "jdbc:postgresql://localhost:5432/postgres";
         String username = "postgres";
-        String password = "********";
+        String password = "***";
 
-        Class.forName("org.postgresql.Driver");
         Connection dbconnection = DriverManager.getConnection(url, username, password);
-
         System.out.println("Connection established " + dbconnection);
+
+        Statement statement = dbconnection.createStatement();
+        ResultSet getAccount = statement.executeQuery(
+                "select * from board"
+        );
+        getAccount.next();
+        System.out.println(getAccount.getString("name"));
+
+        dbconnection.close();
     }
 }
